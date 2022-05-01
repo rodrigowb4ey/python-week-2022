3344aad7e07b1faa101947391c658c34dbb6bf20
Rodrigo Bezerra <rodrigobezerrasaraiva@gmail.com>
2022-05-01 14:08:54 +0000

    Adding CLI

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,15 @@
-from .config import settings
+import typer
+
+main = typer.Typer(help="Beer management app")
 
 
-def main():
-    print("Hello from", settings.NAME)
+@main.command("add")
+def add(name: str, style: str):
+    """Adds a new beer to database"""
+    print(name, style)
+
+
+@main.command("list")
+def list_beers(style: str):
+    """Lists all beers in database"""
+    print(style)
