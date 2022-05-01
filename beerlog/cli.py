77d59ac4f3225429ce86e5ef51a18b8fb486eaf4
Rodrigo Bezerra <rodrigobezerrasaraiva@gmail.com>
2022-05-01 15:50:42 +0000

    Added 'add_beer_to_db' command

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,15 +1,23 @@
 import typer
+from beerlog.core import add_beer_to_db
 
 main = typer.Typer(help="Beer management app")
 
 
 @main.command("add")
-def add(name: str, style: str):
+def add(
+    name: str,
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...),
+):
     """Adds a new beer to database"""
-    print(name, style)
+    if add_beer_to_db(name, style, flavor, image, cost):
+        print("Beer added to database!")
 
 
 @main.command("list")
-def list_beers(style: str):
+def list_beers():
     """Lists all beers in database"""
     print(style)
